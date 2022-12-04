68de7b820b536f841d5a89954afe39f4751d3042
benyq <1520063035@qq.com>
2022-12-04 23:32:21 +0800

    初始化

---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+    <ul class="todo-main">
+        <TodoItem v-for="item in todos" :key="item.id" :todo="item"/>
+    </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+export default {
+    name: 'TodoList',
+    components: { TodoItem },
+    data() {
+        return {
+            todos: [
+                { id: '001', title: '抽烟', done: true },
+                { id: '002', title: '喝酒', done: false },
+                { id: '003', title: '烫头', done: true },
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+.todo-main {
+    margin-top: 15px;
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+}
+
+.todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+}
+</style>
